581dbbd80a9b1613eac680d23c24986579f51c4b
Emma Olson <emma.olson794@gmail.com>
2022-05-20 20:22:52 -0500

    basic department get call working

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const db = require('./db/connection');
+const inputCheck = require('./utils/inputCheck');
 
 // instantiate the server
 const PORT = process.env.PORT || 3001;
@@ -8,6 +9,22 @@ const app = express();
 // Express middleware
 app.use(express.urlencoded({ extended: false })) // parse incoming string or array data
 app.use(express.json()); // parse incoming json data
+
+app.get('/api/department', (req, res) => {
+    const sql = `SELECT * FROM department`;
+
+    db.query(sql, (err, rows) => {
+        if (err) {
+            res.status(500).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: rows
+        });
+    });
+});
+
 
 // Catchall 
 app.use((req, res) => {
