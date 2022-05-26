c9007e548b5103ace3316a9501efa20f585b9f88
Emma Olson <emma.olson794@gmail.com>
2022-05-26 12:47:01 -0500

    CRUD finished on departmentRoutes

--- routes/api/departmentRoutes.js
+++ routes/api/departmentRoutes.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const req = require('express/lib/request');
 const db = require('../../db/connection');
 const inputCheck = require('../../utils/inputCheck');
 
@@ -16,6 +17,67 @@ router.get('/', (req, res) => {
             message: 'success',
             data: rows
         });
+    });
+});
+
+// get a single department   /api/departments/:id
+router.get('/:id', (req, res) => {
+    const sql = `SELECT * FROM department WHERE id = ?`;
+    const params = [req.params.id];
+
+    db.query(sql, params, (err, row) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: row
+        });
+    });
+});
+
+// delete a department  /api/departments/:id
+router.delete('/:id', (req, res) => {
+    const sql = `DELETE FROM department WHERE id =?`;
+    const params = [req.params.id];
+
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.status(400).json({ error: res.message });
+        } else if (!result.affectedRows) {
+            res.json({
+                message: 'Department not found'
+            });
+        } else {
+            res.json({
+                message: 'deleted',
+                changes: result.affectedRows,
+                id: req.params.id
+            });
+        }
+    });
+});
+
+// update a department name  /api/departments/:id
+router.put('/:id', (req, res) => {
+    const sql = `UPDATE department SET name = ? WHERE id = ?`;
+    const params = [req.body.name, req.params.id];
+
+    db.query(sql, params, (err, result) => {
+        if (err) { // check for errors
+            res.status(400).json({ error: err.message });
+        } else if (!result.affectedRows) { // check to see if any rows have been affected
+            res.json({
+                message: 'Department not found'
+            });
+        } else {
+            res.json({
+                message: 'success',
+                data: req.body,
+                changes: result.affectedRows
+            });
+        }
     });
 });
 
